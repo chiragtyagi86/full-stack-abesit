d2e0599e69fddbbfbc2c3975a0de926c604436be
CHIRAG TYAGI <69377702+chiragtyagi86@users.noreply.github.com>
2023-12-20 20:15:35 +0530

    updated

--- front-end/src/Components/Features/Features.jsx
+++ front-end/src/Components/Features/Features.jsx
@@ -1,37 +1,58 @@
-import React from 'react'
-import './Features.css'
+import React from "react";
+import "./Features.css";
 const Features = () => {
   return (
-    <div className='features'>
-     <div className="main">
+    <div className="features">
+      <div className="main">
         <h2>Academics</h2>
         <h1>Embark on a journey of learning!</h1>
         <p>
-        Learning is a never ending process! Start yours with a good experience! Join us to explore your potential and skills!
+          Learning is a never ending process! Start yours with a good
+          experience! Join us to explore your potential and skills!
         </p>
         <ul>
-            <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde, assumenda.</li>
-            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, nisi?</li>
-            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, ducimus!</li>
-            <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque, iusto.</li>
-            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, sed!</li>
-            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, commodi!</li>
+          <li>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde,
+            assumenda.
+          </li>
+          <li>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis,
+            nisi?
+          </li>
+          <li>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut,
+            ducimus!
+          </li>
+          <li>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque,
+            iusto.
+          </li>
+          <li>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque,
+            sed!
+          </li>
+          <li>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio,
+            commodi!
+          </li>
         </ul>
-        </div> 
-     <div className="card">
-      <h2>
-      Apply for
-      </h2>
-      <h1>Admission 2023</h1>
-      <p>applications are now open</p>
-      <button id='feat'>Fee Structure</button>
-
-      <a href="/">Lorem ipsum dolor sit amet.</a>
-      <a href="/">Lorem ipsum dolor sit amet.</a>
-      <button id='feat'>apply online</button>
-     </div>
+      </div>
+      <div className="card">
+      <article class="information [ card ]">
+		<span class="tag">IMPORTANT</span>
+		<h2 class="title">THIS IS A CLONE WEBSITE</h2>
+		<p class="info">This website is made by our team and more information about our team you can check</p>
+		<button class="button">
+			<span>Learn more</span>
+			<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="none">
+				<path d="M0 0h24v24H0V0z" fill="none" />
+				<path d="M16.01 11H4v2h12.01v3L20 12l-3.99-4v3z" fill="currentColor" />
+			</svg>
+		</button>
+	</article>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Features
+export default Features;
--- front-end/src/Components/Hero/Hero.jsx
+++ front-end/src/Components/Hero/Hero.jsx
@@ -12,7 +12,6 @@ const Hero = () => {
       <Features/>
       <Temp/>
       <Reser/>
-      <Students/>
        </div>
   )
 }
